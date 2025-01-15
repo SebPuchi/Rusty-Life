d877db540668c97cf96005f3307086191d923ce5
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-01-15 18:17:31 -0500

    basic clap working

--- rusty_life/src/args.rs
+++ rusty_life/src/args.rs
@@ -2,14 +2,9 @@ use clap::Parser;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long, default_value_t = 200)]
+    pub grid_width: i16,
 }
 
--- rusty_life/src/main.rs
+++ rusty_life/src/main.rs
@@ -1,13 +1,17 @@
 mod grid;
+mod args;
 
 use grid::LifeGrid;
+use args::Args;
 use clap::Parser;
 
 
 fn main() {
     let grid1 = LifeGrid::new(100, 100);
     let grid2 = LifeGrid::new(200, 200);
-    let grid3 = LifeGrid::default();
-    println!("custom_grid: width = {}, height = {}", grid1.width, grid1.height);
-    println!("default_grid: width = {}, height = {}", grid2.width, grid2.height);
+    let args = Args::parse();
+    println!("grid1: width = {}, height = {}", grid1.width, grid1.height);
+    println!("grid2: width = {}, height = {}", grid2.width, grid2.height);
+    println!("width arg = {}", args.grid_width);
+//    println!("height arg = {}", args.grid_height);
 }
