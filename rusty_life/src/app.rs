676b1a0bafdcceaefcc240a13daceabe69a3815c
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2025-01-19 10:47:03 -0500

    basic handle event functions

--- rusty_life/src/app.rs
+++ rusty_life/src/app.rs
@@ -22,7 +22,7 @@ impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
-            //self.handle_events()?;
+            self.handle_events()?;
         }
         Ok(())
     }
@@ -31,9 +31,38 @@ impl App {
         frame.render_widget(self, frame.area());
     }
 
-    //fn handle_events(&mut self) -> io::Result<()> {
-    //    todo!()
-    //}
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+     fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
 }
 
 impl Widget for &App {
